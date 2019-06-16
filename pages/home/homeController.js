873e5035a253a3f172a411f6990e71a6f4eb91bd
lihisho <lihis26@gmail.com>
2019-06-17 00:54:13 +0300

    a good start

--- pages/home/homeController.js
+++ pages/home/homeController.js
@@ -1,9 +1,22 @@
 // home controller
 angular.module("myApp")
-    .controller("homeController", function ($scope) {
+    .controller("homeController", function ($scope, $http) {
         // button click count
         $scope.login = function() {
             $scope.test = "ggg";
 
         }
+
+        $http({
+            method : "POST",
+            url : serverUrl + "/private/users/login",
+            // headers: {},
+            data: {
+                "username": $scope.username,
+                "password":$scope.password
+            }
+          }).then(function mySuccess(response) {
+
+          }, function myError(response) {
+          });
     });
